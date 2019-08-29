9c73dfbfcb690699242c7ea89b3365bd801bb1c7
vitorregisr <vitorregisrr@gmail.com>
2019-08-29 14:17:41 -0300

    Set spinner fullscren height to 100vh

--- src/components/UI/Spinner/Spinner.js
+++ src/components/UI/Spinner/Spinner.js
@@ -5,7 +5,7 @@ import styles from './Spinner.module.scss';
 const Spinner = (props) => {
     return(
         <div style={{ 
-            height: props.fullscreen ? '50vh' : 'unset', 
+            height: props.fullscreen ? '100vh' : 'unset', 
             width: props.fullscreen ? '100vw' : 'unset' 
         }}>
             <div className={styles.Spinner}></div>
